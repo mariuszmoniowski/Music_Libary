541b3ec00c58b8b88dc039f4939895270a5ac7b0
mariuszmoniowski <mariusz.moniowski@gmail.com>
2019-05-27 23:35:04 +0200

    objects 1,2

--- music_reports.py
+++ music_reports.py
@@ -1,6 +1,31 @@
 """responsible for generating reports from given data and loading file 
 (contains main logic of the program)"""
 
+import csv, time
+
+class Album:
+  def __init__(self, artist_name,album_name,release_year,genre,length ):
+    #artist name,album name,release year,genre,length 
+    self.artist_name = artist_name
+    self.album_name = album_name
+    self.release_year = release_year
+    self.genre = genre
+    self.length = length
+  def print_album(self):
+      print("Artist name => " + str(self.artist_name) + ", Album name => " + str(self.album_name)
+       + ", Release year => " + str(self.release_year) + ", Genre => " + str(self.genre)
+        + ", Length => " + str(self.length))
+
+def album_init(file_name):
+    album_list = []
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            album_list.append(Album(row[0],row[1],row[2],row[3],row[4]))
+    return album_list
+            
+
+
 def import_albums_from_file():
     with open("albums.txt") as file:
         all_imported_albums = file.readlines()
@@ -21,6 +46,7 @@ def want_to_find_all_albums_by_genre(single_album_list, genre):
 #artist name,album name,release year,genre,length
 
 def main():
+    """
     genre = "rock"
     stripped_albums = import_albums_from_file()
     single_album_list = want_to_view_all_imported_albums(stripped_albums)    
@@ -28,7 +54,29 @@ def main():
     
     #want_to_view_all_imported_albums(music_data)
     want_to_find_all_albums_by_genre(single_album_list, genre)
+    """
+    album_list = album_init("albums.txt")
+    #print("album_list: " + str(album_list))
+
+    print("1) Genre = rock: ")
+
+    for album in album_list:
+        if album.genre == "rock":
+            album.print_album()
     
 
+    print("2)  Time range: ")
+    time_format = "%M:%S"
+    time_from = "100:00"
+    time_to = "200:00"
+    """
+    for album in album_list:
+        if time.strptime(time_from, time_format) >= time.strptime(album.length, time_format) and time.strptime(time_to, time_format) <= time.strptime(album.length, time_format) :
+            album.print_album()
+    """
+    for album in album_list:
+        
+        if album.length >= time_from  and album.length <= time_to :
+            album.print_album()
 
 main()
---
+++ testy.py
@@ -0,0 +1,5 @@
+import time
+
+print("hello")
+
+print(time.strptime("20:01", '%M:%S'))
