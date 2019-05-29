e0789d4b7d60482cc9ba425b28a4d168bbaa5a8f
Mariusz Moniowski <mariusz.moniowski@gmail.com>
2019-05-29 18:33:37 +0200

    import, by genre, find by time, -->works

--- music_reports.py
+++ music_reports.py
@@ -10,7 +10,6 @@ def import_albums_from_file(): #importuje z pliku i dziele go na podzielone albu
             splited_albums.append(album_as_list)
     return splited_albums
 
-
 #artist name,album name,release year,genre,length
 
 def get_albums_based_on_genre(splited_albums, genre):
@@ -20,30 +19,43 @@ def get_albums_based_on_genre(splited_albums, genre):
             return_list.append(album)
     return return_list
 
-def want_to_find_all_albums_from_given_time_range():
-    return_list = []
-
-    return return_list
 
 
-def my_print(my_list):
+def want_to_find_all_albums_from_given_time_range(splited_albums, convert_lenght_into_sec): #it takes time lenght of album and convert it to seconds
+    #user_lenght = ask_for_time_range()
+    from_time = convert_lenght_into_sec(input("Enter time range 'FROM' (mm:ss): "))
+    to_time = convert_lenght_into_sec(input("Enter time range 'TO' (mm:ss): ")"\n")
+    for element in splited_albums:
+        time = element[4]
+        album_lenght = convert_lenght_into_sec(time) 
+        if album_lenght in range(from_time,to_time+1):
+            print(element)
+        
+
+def convert_lenght_into_sec(time): #it takes any time in format mm:ss and convert it to seconds
+    time_sec = time.split(':')
+    time_sec = (int(time_sec[0]) * 60) + int(time_sec[1])
+    return time_sec
+
+def my_print(my_list): #it prints every element in lists
     for album in my_list:
         print(album)
 
-def main():
+def main(): #runs program
     genre = "rock"
     splited_albums = import_albums_from_file()
     #single_album_list = want_to_view_all_imported_albums(splited_albums)    
         
-    my_print(splited_albums)
+    #my_print(splited_albums)
 
 
-    genre_albums = get_albums_based_on_genre(splited_albums, genre)
-    my_print(genre_albums)
-
+    #genre_albums = get_albums_based_on_genre(splited_albums, genre)
+    #my_print(genre_albums)
     #want_to_view_all_imported_albums(splited_albums)
     #want_to_find_all_albums_by_genre(single_album_list, genre)
     #new_list_with_albums(splited_albums, genre)
     #print(pojedyncze_albumy)
-    
+    want_to_find_all_albums_from_given_time_range(splited_albums, convert_lenght_into_sec)
+     
+
 main()
