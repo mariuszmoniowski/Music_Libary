17d428092f2860dd0e954026b97c36584e5e9518
Mariusz Moniowski <mariusz.moniowski@gmail.com>
2019-05-29 10:18:07 +0200

    clined code in funcions - now it can print by genre

--- music_reports.py
+++ music_reports.py
@@ -1,34 +1,69 @@
 """responsible for generating reports from given data and loading file 
 (contains main logic of the program)"""
 
-def import_albums_from_file():
-    with open("albums.txt") as file:
-        all_imported_albums = file.readlines()
-        stripped_albums = [element.strip().split(",") for element in all_imported_albums]
-    return stripped_albums
+def import_albums_from_file(): #importuje z pliku i dziele go na podzielone albumy
+    with open("albums.txt") as my_file:
+        file_data = my_file.readlines()
+        splited_albums = []
+        for row in file_data:
+            album_as_list = row.strip().split(',')
+            splited_albums.append(album_as_list)
+    return splited_albums
 
-def want_to_view_all_imported_albums(stripped_albums):
-    for single_album_list in stripped_albums:
-        print(single_album_list)
-    return single_album_list
+def want_to_view_all_imported_albums(splited_albums): #chce wydrukowac podzielone albumy
+    for single_album_list in splited_albums:
+        #print(single_album_list)
+        return single_album_list
+
+def pojedyncze_albumy(splited_albums): #sprawdzam czy da sie dodac album do nowej listy
+    aaa = []
+    for pojedynczy_album in splited_albums:
+        aaa.append(pojedynczy_album)
+    return aaa
+
+
     
-def want_to_find_all_albums_by_genre(single_album_list, genre):
-    for category in single_album_list:
-        if category == genre:
-            print(single_album_list)
-
-
+def want_to_find_all_albums_by_genre(single_album_list, genre): #sprawdzam czy da sie 
+    new_list = []
+    for element in single_album_list:
+        new_list.append(element)
+    print(new_list)
 #artist name,album name,release year,genre,length
+
+def new_list_with_albums(splited_albums, genre): #sprawdzam czy da sie dodac do nowej listy po parametrze
+    lista = []
+    for album in splited_albums:
+        if genre in album:
+            lista.append(album)
+        print(lista)
+    return lista
+
+def get_albums_based_on_genre(splited_albums, genre):
+    return_list = []
+    for album in splited_albums:
+        if album[3] == genre:
+            return_list.append(album)
+
+    return return_list
+
+def my_print(my_list):
+    for album in my_list:
+        print(album)
 
 def main():
     genre = "rock"
-    stripped_albums = import_albums_from_file()
-    single_album_list = want_to_view_all_imported_albums(stripped_albums)    
-    music_data = import_albums_from_file()
-    
-    #want_to_view_all_imported_albums(music_data)
-    want_to_find_all_albums_by_genre(single_album_list, genre)
-    
+    splited_albums = import_albums_from_file()
+    #single_album_list = want_to_view_all_imported_albums(splited_albums)    
+        
+    my_print(splited_albums)
 
 
+    genre_albums = get_albums_based_on_genre(splited_albums, genre)
+    my_print(genre_albums)
+
+    #want_to_view_all_imported_albums(splited_albums)
+    #want_to_find_all_albums_by_genre(single_album_list, genre)
+    #new_list_with_albums(splited_albums, genre)
+    #print(pojedyncze_albumy)
+    
 main()
